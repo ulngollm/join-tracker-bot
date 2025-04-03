0dec98479592aed027ef91d3fae8e66b06d138ed
ulngollm <ulngollm@gmail.com>
2025-04-03 11:16:10 +0300

    reactions diff

--- main.go
+++ main.go
@@ -2,10 +2,11 @@ package main
 
 import (
 	"fmt"
-	"github.com/joho/godotenv"
-	tele "gopkg.in/telebot.v4"
 	"log"
 	"os"
+
+	"github.com/joho/godotenv"
+	tele "gopkg.in/telebot.v4"
 )
 
 var (
@@ -50,7 +51,7 @@ func init() {
 
 func main() {
 	bot.Handle(tele.OnMessageReaction, onChatReaction)
-	bot.Handle(tele.OnMessageReactionCount, onChannelReactions)
+	bot.Handle(tele.OnMessageReactionCount, onChannelReactions) // очень медленно отслеживаются
 	bot.Handle(tele.OnChatMember, handleJoin)
 
 	bot.Start()
@@ -64,6 +65,7 @@ func handleJoin(c tele.Context) error {
 	//возможно никак. OldChatMember есть и у новых пользователей
 	userID := c.ChatMember().Sender.ID
 	t := c.ChatMember().Time()
+	//todo проверять, что пользователь впервые добавляется в чат?
 	if err := joinHandler.LogJoin(c.Chat(), userID, t); err != nil {
 		log.Printf("logJoin: %s", err)
 	}
@@ -72,11 +74,46 @@ func handleJoin(c tele.Context) error {
 }
 
 func onChannelReactions(c tele.Context) error {
-	fmt.Printf("add %d %s reactions to chat %s", c.Update().MessageReactionCount.Reactions[0].Count, c.Update().MessageReactionCount.Reactions[0].Type.Emoji, c.Update().MessageReactionCount.Chat.Title)
+	fmt.Printf("%v\n", c.Update().MessageReactionCount)
+	fmt.Printf("add %d %s reactions to chat %s\n", c.Update().MessageReactionCount.Reactions[0].Count, c.Update().MessageReactionCount.Reactions[0].Type.Emoji, c.Update().MessageReactionCount.Chat.Title)
 	return nil
 }
 
 func onChatReaction(c tele.Context) error {
-	fmt.Printf("%s reaction from user %s to chat %s", c.Update().MessageReaction.NewReaction[0].Emoji, c.Update().MessageReaction.User.Username, c.Update().MessageReaction.Chat.Title)
+	r := c.Update().MessageReaction
+	var t string
+	if len(r.NewReaction) < len(r.OldReaction) {
+		t = "remove"
+	} else {
+		t = "add"
+	}
+
+	reactions := map[string]bool{}
+	for _, reaction := range r.OldReaction {
+		reactions[reaction.Emoji] = t == "remove"
+	}
+	for _, reaction := range r.NewReaction {
+		if t == "add" {
+			if _, ok := reactions[reaction.Emoji]; !ok {
+				reactions[reaction.Emoji] = true
+			}
+		} else {
+			reactions[reaction.Emoji] = false
+		}
+	}
+	emojis := ""
+	for reaction, changed := range reactions {
+		if changed {
+			emojis += reaction
+		}
+	}
+	var username string
+	if r.User != nil {
+		username = r.User.Username
+	}
+	if r.ActorChat != nil {
+		username = r.ActorChat.Title
+	}
+	fmt.Printf("%s %s reaction from %s to chat %s\n", t, emojis, username, r.Chat.Title)
 	return nil
 }
